0c4479fe0049c292029e6f6f5d276cb7e8de1c3f
giantneco <giantneco@gmail.com>
2014-01-12 15:40:17 +0900

    Modify

--- project.clj
+++ project.clj
@@ -3,4 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :jvm-opts ["-Dfile.encoding=UTF-8"]
+  :main sandbox.core
+  :dependencies [[org.clojure/clojure "1.5.1"]
+		 [com.cemerick/pomegranate "0.2.0"]
+		 [antler/xyzzy "0.1.0"]
+		 [enlive "1.1.5"]
+		 ;; [crouton "0.1.1"]
+])
+
--- src/sandbox/core.clj
+++ src/sandbox/core.clj
@@ -1,6 +1,52 @@
 (ns sandbox.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+; read 'README.md' and print lines
+;; (with-open [r (clojure.java.io/reader "README.md")] 
+;;   (doseq [line (line-seq r)]
+;;     (println line)))
+
+;; (clojure.string/capitalize "capitalize")
+
+;; (clojure.string/upper-case "upper case")
+
+;; (clojure.string/lower-case "LOWER CASE")
+
+;; (use '[cemerick.pomegranate :only (add-dependencies)])
+
+(defn recur-fibo [n]
+  (letfn [(fib [current next n]
+            (if (<= n 0)
+              current
+              (recur next (+ current next) (dec n))))]
+    (fib 0N 1N n)))
+
+(defn lazy-seq-fibo
+  ([]
+     (concat [0 1] (lazy-seq-fibo 0N 1N)))
+  ([a b]
+     (let [n (+ a b)]
+       (lazy-seq
+        (cons n (lazy-seq-fibo b n))))))
+
+(defn fibo []
+  (map first
+       (iterate (fn [[a b]] [b (+ a b)]) [0N 1N])))
+
+(defn show-hello-world
+  "show hello world"
+  []
+  (println "Hello World"))
+
+(defn read-and-write
+  "read lines from stdin and write them to stdout"
+  []
+  (doall
+   (map println
+        (line-seq (java.io.BufferedReader. *in*)))))
+
+(defn -main
+  "main function"
+  []
+  (show-hello-world)
+  (read-and-write)
+  )
